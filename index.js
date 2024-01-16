5099252087815d0231e697bb2879caba735b245e
sihyeon5623 <86709085+sihyeon5623@users.noreply.github.com>
2024-01-17 03:35:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [359550, 105600, 730, 578080, 1144200, 1091500,1245620,648800,457140];  // Enter here AppIDs of the needed games
+var games = [730, 440, 570];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
-// 레식 테라리아  카스2 배그 레오낫 사펑 엘든링  레프트 산미
+
 
 user = new steamUser();
 user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
@@ -17,20 +17,3 @@ user.on('loggedOn', () => {
 	user.setPersona(status);               
 	user.gamesPlayed(games);
 });
-
-
-// var username2 = process.env.username2;
-// var password2 = process.env.password2;
-// var shared_secret2 = process.env.shared2;
-
-// var games2 = [730, 440, 570, 304930];  // Enter here AppIDs of the needed games
-// var status2 = 1;  // 1 - online, 7 - invisible
-
-
-// user2 = new steamUser();
-// user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
-// user2.on('loggedOn', () => {
-// 	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
-// 	user2.setPersona(status2);               
-// 	user2.gamesPlayed(games2);
-// });
