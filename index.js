83f1966dd4e56436a3d3b13449059385a2df9711
sihyeon5623 <86709085+sihyeon5623@users.noreply.github.com>
2024-01-29 01:30:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [359550, 105600, 1281930, 730, 578080, 1144200, 1091500, 1245620, 457140, 555160, 250900, 367520];  // Enter here AppIDs of the needed games
-var status = 7;  // 1 - online, 7 - invisible
-// 레식 테라리아 티모드로더 카스2 배그 레오낫 사펑 엘든링 산미 파블로프 할로우
+var games = [367520, 359550, 105600, 1281930, 730, 578080, 1144200, 1091500, 1245620, 457140, 555160, 250900];  // Enter here AppIDs of the needed games
+var status = 1;  // 1 - online, 7 - invisible
+//할로우 레식 테라리아 티모드로더 카스2 배그 레오낫 사펑 엘든링 산미 파블로프 
 
 user = new steamUser();
 user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
