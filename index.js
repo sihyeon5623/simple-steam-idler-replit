4a6ffef50a3ac64d7db218c1687e0ddf64f09441
sihyeon5623 <86709085+sihyeon5623@users.noreply.github.com>
2024-03-15 00:34:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [1174180, 359550, 105600, 1281930, 730, 1144200, 1091500, 1245620, 555160, 2357570];  // Enter here AppIDs of the needed games
+var games = [1174180, 359550, 105600, 1281930, 730, 1144200, 1091500, 1245620, 555160, 2357570, 553850];  // Enter here AppIDs of the needed games
 var status = 7;  // 1 - online, 7 - invisible
 //레데리  레식 테라리아 티모드로더 카스2  레오낫 사펑 엘든링  파블로프 오버워치
 
