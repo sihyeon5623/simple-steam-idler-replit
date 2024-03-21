44f3014c0fa18e0e9796bbb8d0257a353c51c2c0
sihyeon5623 <86709085+sihyeon5623@users.noreply.github.com>
2024-03-22 01:16:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [1174180, 359550, 105600, 1281930, 730, 1144200, 1091500, 1245620, 555160, 2357570, 553850];  // Enter here AppIDs of the needed games
+var games = [1174180, 359550, 105600, 1281930, 730, 1144200, 1091500, 1245620, 553850, 346110];  // Enter here AppIDs of the needed games
 var status = 7;  // 1 - online, 7 - invisible
-//레데리  레식 테라리아 티모드로더 카스2  레오낫 사펑 엘든링  파블로프 오버워치
+//레데리  레식 테라리아 티모드로더 카스2  레오낫 사펑 엘든링 헬다이버 ㅏ
 
 user = new steamUser();
 user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
