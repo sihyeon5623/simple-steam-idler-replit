f94d7156ec17063917851801323a858836b9d88e
sihyeon5623 <86709085+sihyeon5623@users.noreply.github.com>
2024-04-09 00:57:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [1174180, 359550, 105600, 1281930, 730, 1144200, 1091500, 1245620, 553850, 346110];  // Enter here AppIDs of the needed games
+var games = [1174180, 359550, 105600, 1281930, 730, 1144200, 1091500, 1245620, 553850, 346110, 8500, 1966720];  // Enter here AppIDs of the needed games
 var status = 7;  // 1 - online, 7 - invisible
-//레데리  레식 테라리아 티모드로더 카스2  레오낫 사펑 엘든링 헬다이버 ㅏ
+//레데리  레식 테라리아 티모드로더 카스2  레오낫 사펑 엘든링 헬다이버 아크 이브 리썰
 
 user = new steamUser();
 user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
